f5cd8b1acb419c55df837ea5745bb26763fd1ccf
tazkiajessy <jessy566eee@gmail.com>
2021-06-14 11:04:29 +0200

    endpoint registration

--- server/index.js
+++ server/index.js
@@ -1,24 +1,39 @@
-const express = require("express"); //import express
-const cors = require("cors"); //import cors
+const express = require("express");
+const loggerMiddleWare = require("morgan");
+const { PORT } = require("./config/constants");
 
-const userRouter = require("./routers/user"); //import user router
+const authRouter = require("./routers/auth"); //import user router
 const categoryRouter = require("./routers/category");
 const productRouter = require("./routers/product");
+const orderRouter = require("./routers/order");
+const couponRouter = require("./routers/coupon");
 
-const port = 4000;
 const app = express(); //create an express server
 
-//middlewares
-const jsonParser = express.json();
-app.use(jsonParser);
+const cors = require("cors");
 app.use(cors());
 
+//middlewares
+
+app.use(loggerMiddleWare("dev"));
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+if (process.env.DELAY) {
+  app.use((req, res, next) => {
+    setTimeout(() => next(), parseInt(process.env.DELAY));
+  });
+}
+
 //Registering the router to the app
-app.use("/users", userRouter);
+app.use("/", authRouter);
 app.use("/categories", categoryRouter);
 app.use("/products", productRouter);
+app.use("/orders", orderRouter);
+app.use("/coupons", couponRouter);
 
 //start server
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
 });

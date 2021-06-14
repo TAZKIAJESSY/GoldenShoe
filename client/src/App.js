c266a9f5b00ff41b7b79e05437a695ed0c264e9d
tazkiajessy <jessy566eee@gmail.com>
2021-06-14 11:07:05 +0200

    new route

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+
+// import "./App.css";
+import Navigation from "./component/Navigation";
+import CartPage from "./pages/CartPage";
+import DetailsPage from "./pages/DetailsPage";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/LoginPage";
+import MyOrderPage from "./pages/MyOrderPage";
+import SignUp from "./pages/SignUpPage";
+import UserOrderFormPage from "./pages/UserOrderFromPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/details/:id" component={DetailsPage} />
+        <Route path="/cart" component={CartPage} />
+        <Route path="/orderform" component={UserOrderFormPage} />
+        <Route path="/myorder" component={MyOrderPage} />
+
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
     </div>
   );
 }

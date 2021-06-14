35d640581128198b14ec5b123751d7dc28a312bb
tazkiajessy <jessy566eee@gmail.com>
2021-06-14 11:10:38 +0200

    log in component created

---
+++ client/src/component/LoggedIn.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Button, Nav } from "react-bootstrap";
+
+import { selectUser } from "../store/user/selectors";
+import { logOut } from "../store/user/actions";
+
+export default function LoggedIn() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  return (
+    <>
+      <Nav.Item style={{ padding: ".5rem 1rem" }}>
+        <i style={{ color: "white" }}> Hello</i>{" "}
+        <b style={{ color: "yellow" }}> 🌸 {user.firstName}</b>
+      </Nav.Item>
+      <Button onClick={() => dispatch(logOut())}>Logout</Button>
+    </>
+  );
+}

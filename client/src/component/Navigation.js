d6b940053070138f5e444ac735d19cfac770b0f9
tazkiajessy <jessy566eee@gmail.com>
2021-06-14 11:09:36 +0200

    navigation for user login

--- client/src/component/Navigation.js
+++ client/src/component/Navigation.js
@@ -1,20 +1,31 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { selectToken, selectUser } from "../store/user/selectors";
+import LoggedIn from "./LoggedIn";
+
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { NavLink } from "react-router-dom";
 import godenShoe from "../Assets/Golden Shoe1.png";
 
 export default function Navigation() {
+  const token = useSelector(selectToken);
+  const user = useSelector(selectUser);
+
   return (
     <Navbar bg="dark" variant={"dark"} expand="lg">
       <Navbar.Brand>
-        <img
-          src={godenShoe}
-          width="160"
-          height="85"
-          className="d-inline-block align-top"
-          alt="golden shoe"
-        />
+        <Link to={`/`}>
+          {" "}
+          <img
+            src={godenShoe}
+            width="160"
+            height="85"
+            className="d-inline-block align-top"
+            alt="golden shoe"
+          />
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse
@@ -30,9 +41,22 @@ export default function Navigation() {
           <Nav.Link exact as={NavLink} to={"/"}>
             Home
           </Nav.Link>
-          <Nav.Link as={NavLink} to={"/details"}>
-            Details
+
+          <Nav.Link as={NavLink} to={"/cart"}>
+            🛒
           </Nav.Link>
+
+          <Nav.Link as={NavLink} to={"/myorder"}>
+            My Order
+          </Nav.Link>
+
+          {token ? (
+            <LoggedIn />
+          ) : (
+            <Nav.Link as={NavLink} to={"/login"}>
+              Login
+            </Nav.Link>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>

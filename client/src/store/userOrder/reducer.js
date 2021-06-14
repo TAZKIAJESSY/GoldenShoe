da02e2b29f259501ba4bcc160f865b147cb1b9e8
tazkiajessy <jessy566eee@gmail.com>
2021-06-14 11:06:46 +0200

    reducer for user orders

---
+++ client/src/store/userOrder/reducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+  loading: false,
+  orders: [],
+};
+
+export default function productSliceReducer(state = initialState, action) {
+  switch (action.type) {
+    case "userOrder/startLoading": {
+      return { ...state, loading: true };
+    }
+    case "userOrder/fetchedOrder": {
+      return {
+        ...state,
+        loading: false,
+        orders: action.payload,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}

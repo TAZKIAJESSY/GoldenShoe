de7d4e62e854e3bd95f02b500cfffebacf263a02
tazkiajessy <jessy566eee@gmail.com>
2021-06-16 13:46:46 +0200

    for heroku deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:4000";
+// export const API_URL = "http://localhost:4000";
+
+export const API_URL = "https://golden-shoe-a.herokuapp.com/"; ////for heroku deploys

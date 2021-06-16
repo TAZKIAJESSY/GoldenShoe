4f3c67ca98eeb3080c145111eb20d463b008bb54
tazkiajessy <jessy566eee@gmail.com>
2021-06-16 13:25:36 +0200

    removed extra import

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useHistory, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Spinner } from "react-bootstrap";
